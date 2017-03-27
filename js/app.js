318be76114f5f085bbb5859f0ced63e6fd7d188e
Donny <EDL1155@gmail.com>
2017-03-27 09:48:13 -0700

    new updates

--- js/app.js
+++ js/app.js
@@ -1,11 +1,39 @@
 $( document ).ready(function(){
 console.log(document.getElementById('my-portfolio'))
 
-var waypoint = new Waypoint({
-  element: document.getElementById('my-portfolio'),
+var portfolioWaypoint = new Waypoint({
+  element: $('#my-portfolio'),
   handler: function() {
-   notify('Basic waypoint triggered')
+   $('#nav-portfolio').css({color: "black"})
   }
 })
+
+var aboutWaypoint = new Waypoint({
+  element: $('#about'),
+  handler: function() {
+   $('#nav-about').css({color: "black"})
+  }
+})
+
+var contactWaypoint = new Waypoint({
+  element: $('#contact'),
+  handler: function(direction) {
+	if (direction === "down") {
+		   $('#nav-contact').css({color: "black"})
+		   $('#nav-about').css({color: "white"})
+		   $('#nav-portfolio').css({color: "white"})
+	}
+	else {
+//		  make projects black and others white
+	}
+  }
+})
+
+// if scrolling down 
+// 	we highlight the current page
+//	unhighlight all others
+
+
+
 
 });
---
+++ js/rslides.js
@@ -0,0 +1,6 @@
+
+
+
+ $(function() {
+    $(".rslides").responsiveSlides({speed: 3000 });
+  });
